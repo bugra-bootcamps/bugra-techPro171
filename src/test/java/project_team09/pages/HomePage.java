dde47d8801fac54689134dc70341412aadd1d247
Mesut Erdogan <mrdogan86@gmail.com>
2023-09-19 20:46:18 +0300

    Mesut 19.09.23 V1

---
+++ src/test/java/project_team09/pages/HomePage.java
@@ -0,0 +1,14 @@
+package project_team09.pages;
+
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import project_team09.utilities.Driver;
+
+public class HomePage {
+    public HomePage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+}
